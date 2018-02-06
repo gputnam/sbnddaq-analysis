3fc91d93460e0785af47f0c8f9e68edbab1aee95
gputnam <grayputnam@uchicago.edu>
2018-02-06 14:28:31 -0600

    better fft plotter

--- fft.py
+++ fft.py
@@ -10,20 +10,25 @@ def main(args):
     header_tree = fft_data_file.Get("nevis_header") 
     header_tree.GetEntry(args.entry)
 
-    data = fft_data.channel_data[args.channel].fft_real
+    real = fft_data.channel_data[args.channel].fft_real
+    imag = fft_data.channel_data[args.channel].fft_imag
 
     graph_title = "Event %i Channel %i FFT" % (header_tree.header_data.event_number, args.channel)
-    plot(data, args.output, graph_title)
+    plot(real, imag, args.output, graph_title, not args.keep_baseline)
 
-def plot(fft_data, output_name, graph_title):
-    n_data = len(fft_data)
+def plot(fft_real, fft_imag, output_name, graph_title, ignore_baseline):
+    skip = int(ignore_baseline)
+    n_data = len(fft_real) - skip
 
     fft_data_array = array('d')
     freq_array = array('d')
-    for i,d in enumerate(fft_data):
+    for i,(re,im) in enumerate(zip(fft_real, fft_imag)):
+        if i < skip:
+            continue
+        d = (re*re + im*im)**(1./2.)
         fft_data_array.append(d)
-        freq_array.append(float(i))
-    
+        freq_array.append(float(i+skip))
+
     canvas = ROOT.TCanvas("canvas", "Waveform Canvas", 250,100,700,500)
 
     graph = ROOT.TGraph(n_data, freq_array, fft_data_array)
@@ -44,5 +49,6 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output", default="fft")
     parser.add_argument("-c", "--channel", type=int, default=0)
     parser.add_argument("-e", "--entry", type=int, default=0)
+    parser.add_argument("-b", "--keep_baseline", action="store_true")
     
     main(parser.parse_args())
